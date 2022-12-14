3e85cc3c08637eee6c3dd0c47950d6a1ab4e01cf
toan.tranduc <toan.tranduc@vti.com.vn>
2022-12-14 16:19:01 +0700

    deploy

---
+++ dist/js/about.e34f0fa9.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkrandom_team"]=self["webpackChunkrandom_team"]||[]).push([[443],{7381:function(n,a,t){t.r(a),t.d(a,{default:function(){return f}});var e=t(3396);const r={class:"about"},u=(0,e._)("h1",null,"This is an about page",-1),s=[u];function c(n,a){return(0,e.wg)(),(0,e.iD)("div",r,s)}var o=t(89);const i={},d=(0,o.Z)(i,[["render",c]]);var f=d}}]);
+//# sourceMappingURL=about.e34f0fa9.js.map
